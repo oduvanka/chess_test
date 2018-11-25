fee2c4d50981df6da3fa4f01283af8fa98d5df74
oduvanka <qcatjul@gmail.com>
2018-11-25 22:03:58 +0300

    Фигуры ходят поочерёдно только по подсвеченным клеткам, но пока не могут есть друг друга

--- js/script.js
+++ js/script.js
@@ -1,16 +1,86 @@
-var cages = document.querySelectorAll('.cage');
+var figures = document.querySelectorAll('.contentFigure');
 var dragged;
 var game = new Chess();
 
+
+var removeGreySquares = function() {
+	//удалим подсветку с клеток
+	
+	var squaresLegalW = document.querySelectorAll('.whiteCageLegal');
+	var squaresLegalB = document.querySelectorAll('.blackCageLegal');
+	
+	for (var i=0; i<=squaresLegalW.length-1; i++) {
+		squaresLegalW[i].classList.remove('whiteCageLegal');
+	}
+	for (var j=0; j<=squaresLegalB.length-1; j++) {
+		squaresLegalB[j].classList.remove('blackCageLegal');
+	}
+};
+
+var greySquare = function(square) {
+	//подсветим клетку, на которую можно походить
+	var squareLegal = document.getElementById('cage-' + square);
+	var background = 'whiteCageLegal';
+	if (squareLegal.classList.contains('blackCage')) {
+		background = 'blackCageLegal';
+	}
+	squareLegal.classList.add(background);
+};
+
+var addCageMouseOver = function (figure) {
+	//при наведении на фигуру проверим вызовем подсветку доступных для перемещения клеткок
+	figure.addEventListener("mouseover", function() {
+	
+		var square = event.target.parentNode.dataset.square;
+		
+		// get list of possible moves for this cage
+		var moves = game.moves({
+			square: square,
+			verbose: true
+		});
+		
+		// exit if there are no moves available for this square
+		if (moves.length === 0) return;
+		
+		// highlight the square they moused over
+		greySquare(square);
+		
+		// highlight the possible squares for this piece
+		for (var i = 0; i < moves.length; i++) {
+			greySquare(moves[i].to);
+		}
+  });
+};
+
+var addCageMouseOut = function (figure) {
+	//при сходе курсора с клетки, на которой фигура, вызовем удаление подсветки клеток
+	figure.addEventListener("mouseout", function() {
+		removeGreySquares();
+	});
+};
+
+
+
+var addFigureDragStart = function (figure) {
+	figure.addEventListener("dragstart", function() {	
+		var piece = figure.dataset.piece;
+		
+		// do not pick up pieces if the game is over
+		// or if it's not that side's turn
+		if (game.game_over() === true ||
+			(game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+			(game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+			return false;
+		}
+		else {
+			dragged = event.target;
+			event.target.style.opacity = 0.1;
+		}
+	});
+};
+
 /* events fired on the draggable target */
 document.addEventListener("drag", function(evt) {
-}, false);
-
-document.addEventListener("dragstart", function(evt) {
-	// store a ref. on the dragged elem
-	dragged = evt.target;
-	// make it half transparent
-	evt.target.style.opacity = 0.5;
 }, false);
 
 document.addEventListener("dragend", function(evt) {
@@ -27,70 +97,54 @@ document.addEventListener("dragover", function(evt) {
 document.addEventListener("dragenter", function(evt) {
 	// highlight potential drop target when the draggable element enters it
 	if (~evt.target.id.indexOf("cage-")) {
-		evt.target.style.background = "purple";
+		//evt.target.style.background = "purple";
 	}
 }, false);
 
 document.addEventListener("dragleave", function(evt) {
 	// reset background of potential drop target when the draggable element leaves it
 	if ( ~evt.target.id.indexOf("cage-") ) {
-		evt.target.style.background = "";
+		//evt.target.style.background = "";
 	}
 }, false);
 
 document.addEventListener("drop", function(evt) {
 	// prevent default action (open as link for some elements)
 	evt.preventDefault();
-	// move dragged elem to the selected drop target
-	if ( ~evt.target.id.indexOf("cage-") ) {
-		evt.target.style.background = "";
-		dragged.parentNode.removeChild(dragged);
-		evt.target.appendChild(dragged);
+	
+	evt.target.style.opacity = "";
+	removeGreySquares();
+		
+	// see if the move is legal
+	var move = game.move({
+		from: dragged.parentNode.dataset.square,
+		to: evt.target.dataset.square,
+		promotion: 'q' // NOTE: always promote to a queen for example simplicity
+	});
+
+	// illegal move
+	if (move === null) {
+		//return 'snapback';
+	}
+	else {
+		// move dragged elem to the selected drop target
+		if ( ~evt.target.id.indexOf("cage-") ) {
+			evt.target.style.background = "";
+			dragged.parentNode.removeChild(dragged);
+			evt.target.appendChild(dragged);
+		}
 	}
 }, false);
 
 
-var removeGreySquares = function() {
-  /*$('.cage').css('background', '');*/
-};
-
-var greySquare = function(square) {
-	var background = "rgba(0, 0,0, 0.5)";
-	var squareEl = document.getElementById('cage-' + square);
-	squareEl.style.background = background;
-};
+/*
+var onSnapEnd = function() {
+	//board.position(game.fen());
+};*/
 
 
-
-
-var addCageMouseOver = function (cage) {
-  cage.addEventListener("mouseover", function() {
-	// get list of possible moves for this cage
-	var moves = game.moves({
-		square: cage.dataset.square,
-		verbose: true
-	});
-	
-	// exit if there are no moves available for this square
-	if (moves.length === 0) return;
-	
-	// highlight the square they moused over
-	greySquare(cage.dataset.square);
-	
-	// highlight the possible squares for this piece
-	for (var i = 0; i < moves.length; i++) {
-		greySquare(moves[i].to);
-	}
-  });
-};
-
-var addCageMouseOut = function (cage) {
-  cage.addEventListener("mouseout", function() {
-	removeGreySquares();
-  });
-};
-
-for (var i = 0; i < cages.length; i++) {
-	addCageMouseOver(cages[i]);
-	addCageMouseOut(cages[i]);
+for (var i = 0; i < figures.length; i++) {
+	addCageMouseOver(figures[i]);
+	addCageMouseOut(figures[i]);
+	addFigureDragStart(figures[i]);
 }
